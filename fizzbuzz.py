000acabeb1fb4e5609bc55c28f24c3df4729f470
Claude Roux <52529857+ClaudeRoux@users.noreply.github.com>
2025-05-28 11:07:02 +0200

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,26 @@
+from predicate import p_check, predicate
+
+@predicate()
+def fizzbuzz(i):
+    p_check(i%15==0)
+    print("fizzbuzz", end = " ")
+
+
+@predicate()
+def fizzbuzz(i):
+    p_check(i%3==0)
+    print("fizz", end = " ")
+
+@predicate()
+def fizzbuzz(i):
+    p_check(i%5==0)
+    print("buzz", end = " ")
+
+
+@predicate()
+def fizzbuzz(i):
+    print(i, end = " ")
+
+for i in range(1,200,1):
+    fizzbuzz(i)
+print()
